9d982fbbe2ba9393503428337ece630881d123a5
DavidAHudec <78866967+DavidAHudec@users.noreply.github.com>
2023-11-14 15:25:44 -0500

    Create Menu.ps1

---
+++ Menu.ps1
@@ -0,0 +1,27 @@
+Write-Host  -ForegroundColor Yellow "Starting Custom OSDCloud ..."
+cls
+Write-Host "===================== Main Menu =======================" -ForegroundColor Yellow
+Write-Host "===================== Written by =======================" -ForegroundColor Yellow
+Write-Host "================ www.brookspeppin.com =================" -ForegroundColor Yellow
+Write-Host "====================@brookspeppin =====================" -ForegroundColor Yellow
+Write-Host "=======================================================" -ForegroundColor Yellow
+Write-Host "1: Zero-Touch Win11 22H1 | English | Pro"-ForegroundColor Yellow
+Write-Host "2: Zero-Touch Win11 22H2 | English | Pro" -ForegroundColor Yellow
+Write-Host "3: I'll select it myself"-ForegroundColor Yellow
+Write-Host "4: Exit`n"-ForegroundColor Yellow
+$input = Read-Host "Please make a selection"
+
+Write-Host  -ForegroundColor Yellow "Loading OSDCloud..."
+
+Import-Module OSD -Force
+Install-Module OSD -Force
+
+switch ($input)
+{
+    '1' { Start-OSDCloud -OSLanguage en-us -OSVersion 'Windows 11' -OSBuild 22H1 -OSEdition Pro -ZTI } 
+    '2' { Start-OSDCloud -OSLanguage en-us -OSVersion 'Windows 11' -OSBuild 22H2 -OSEdition Pro -ZTI } 
+    '3' { Start-OSDCloud	} 
+    '4' { Exit		}
+}
+
+wpeutil reboot
