43fcbee44c44d5f0ba37968fed9bbbf9d06ef924
DavidAHudec <78866967+DavidAHudec@users.noreply.github.com>
2024-06-12 14:16:41 -0400

    Update Menu.ps1

--- Menu.ps1
+++ Menu.ps1
@@ -5,8 +5,8 @@ Write-Host "===================== Written by =======================" -Foregroun
 Write-Host "================ www.brookspeppin.com =================" -ForegroundColor Yellow
 Write-Host "====================@brookspeppin =====================" -ForegroundColor Yellow
 Write-Host "=======================================================" -ForegroundColor Yellow
-Write-Host "1: Zero-Touch Win11 22H1 | English | Pro"-ForegroundColor Yellow
-Write-Host "2: Zero-Touch Win11 22H2 | English | Pro" -ForegroundColor Yellow
+Write-Host "1: Zero-Touch Win10 22H2 | English | Pro (Older Machines)"-ForegroundColor Yellow
+Write-Host "2: Zero-Touch Win11 23H2 | English | Pro (Newer Machines)" -ForegroundColor Yellow
 Write-Host "3: I'll select it myself"-ForegroundColor Yellow
 Write-Host "4: Exit`n"-ForegroundColor Yellow
 $input = Read-Host "Please make a selection"
@@ -18,8 +18,8 @@ Install-Module OSD -Force
 
 switch ($input)
 {
-    '1' { Start-OSDCloud -OSLanguage en-us -OSVersion 'Windows 11' -OSBuild 22H1 -OSEdition Pro -ZTI } 
-    '2' { Start-OSDCloud -OSLanguage en-us -OSVersion 'Windows 11' -OSBuild 22H2 -OSEdition Pro -ZTI } 
+    '1' { Start-OSDCloud -OSLanguage en-us -OSVersion 'Windows 10' -OSBuild 22H2 -OSEdition Pro -ZTI } 
+    '2' { Start-OSDCloud -OSLanguage en-us -OSVersion 'Windows 11' -OSBuild 23H2 -OSEdition Pro -ZTI } 
     '3' { Start-OSDCloud	} 
     '4' { Exit		}
 }
